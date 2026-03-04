a28ebeee4f38556bb4d6c1409e353a3f3c721a13
pavelsavara <pavel.savara@gmail.com>
2026-03-04 14:10:27 +0100

    net10

--- src/empty-blazor/Program.cs
+++ src/empty-blazor/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using empty_blazor;
+using EmptyBlazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
