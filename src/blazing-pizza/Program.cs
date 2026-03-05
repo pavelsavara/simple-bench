424a941250a8251c4476fdb96262d561170a4213
pavelsavara <pavel.savara@gmail.com>
2026-03-05 12:02:43 +0100

    fix blazing

--- src/blazing-pizza/Program.cs
+++ src/blazing-pizza/Program.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.Components.Authorization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
+builder.RootComponents.Add<BlazingPizza.Client.Routes>("#app");
+builder.RootComponents.Add<Microsoft.AspNetCore.Components.Web.HeadOutlet>("head::after");
+
 builder.Services.AddScoped<IRepository, InMemoryRepository>();
 builder.Services.AddScoped<OrderState>();
 
