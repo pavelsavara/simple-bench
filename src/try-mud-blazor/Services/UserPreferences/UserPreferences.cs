341ceb930c038dc72e89c55c9931b82489e363d9
pavelsavara <pavel.savara@gmail.com>
2026-03-05 11:41:45 +0100

    kill mud

--- src/try-mud-blazor/Services/UserPreferences/UserPreferences.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TryMudBlazor.Client.Services.UserPreferences;
-
-public class UserPreferences
-{
-    /// <summary>
-    /// If true DarkTheme is used. LightTheme otherwise
-    /// </summary>
-    public bool DarkTheme { get; set; }
-}
